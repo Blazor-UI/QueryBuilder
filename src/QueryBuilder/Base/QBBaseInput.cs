bfe2c806947a359a4680274ff0ba1d0dbb9d6b7d
Adrien Granger <adrien.granger@outlook.com>
2023-11-22 17:02:37 +0100

    WIP - Added abstract input base

---
+++ src/QueryBuilder/Base/QBBaseInput.cs
@@ -0,0 +1,19 @@
+﻿namespace QueryBuilder.Base;
+
+using Microsoft.AspNetCore.Components;
+
+public abstract class QBBaseInput : ComponentBase
+{
+    /// <summary>
+    ///     User class name, separated by space
+    /// </summary>
+    [Parameter]
+    public string? Class { get; set; }
+
+    /// <summary>
+    ///     InputAttributes carries all attributes you add to the component that don't match any of its parameters.
+    ///     They will be splatted onto the underlying HTML tag.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public Dictionary<string, object?> InputAttributes { get; set; } = [];
+}
